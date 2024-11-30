ffc7f926fa4ef3d026dde1ac781c07f1f10eab25
jun7343 <zlsl7343@gmail.com>
2024-11-30 17:30:37 +0900

    UPDATE: gradle 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,53 +1,44 @@
 plugins {
-    kotlin("jvm") version "1.9.24"
-    kotlin("kapt") version "2.0.0"
-    /*
-     * If you use Spring, you can enable the kotlin-spring compiler plugin instead of specifying Spring annotations manually.
-     * The kotlin-spring is a wrapper on top of all-open, and it behaves exactly the same way.
-     */
-    kotlin("plugin.spring") version "1.9.24"
-    kotlin("plugin.jpa") version "1.9.24"
-    id("org.springframework.boot") version "3.2.7"
-    id("io.spring.dependency-management") version "1.1.3"
+    kotlin("jvm") version "2.1.0"
+    kotlin("kapt") version "2.1.0"
 }
+
+allprojects {
+    repositories {
+        mavenCentral()
+    }
+}
+
+val fasterxmlVersion = "2.18.2"
+val jetbrainsKotlinVersion = "2.1.0"
 
 group = "org.example"
-val fasterxmlVersion = "2.17.1"
-val jetbrainsKotlinStdlibVersion = "2.0.0"
-val ulidCreatorVersion = "5.2.3"
-val lineJdslSpringStarterVersion = "2.2.1.RELEASE"
-val jetbrainsKotlinReflectVersion = "2.0.0"
-val kotlinTestVersion = "2.0.0"
-val queryDslVersion = "5.0.0"
 
-repositories {
-    mavenCentral()
-}
+subprojects {
 
-dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("com.h2database:h2")
-    /*
-     * kotlin
-     */
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$fasterxmlVersion")
-    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$fasterxmlVersion")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:$jetbrainsKotlinStdlibVersion")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:$jetbrainsKotlinReflectVersion")
-    implementation("com.auth0:java-jwt:4.4.0")
-}
+    apply {
+        plugin("org.jetbrains.kotlin.jvm")
+        plugin("org.jetbrains.kotlin.kapt")
+    }
 
-tasks.test {
-    useJUnitPlatform()
-}
-kotlin {
-    jvmToolchain(21)
-}
+    dependencies {
+        /*
+         * kotlin
+         */
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$fasterxmlVersion")
+        implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$fasterxmlVersion")
+        implementation("org.jetbrains.kotlin:kotlin-stdlib:$jetbrainsKotlinVersion")
+        implementation("org.jetbrains.kotlin:kotlin-reflect:$jetbrainsKotlinVersion")
+    }
 
-kotlin {
-    jvmToolchain(21)
-}
+    tasks.test {
+        useJUnitPlatform()
+    }
+    kotlin {
+        jvmToolchain(21)
+    }
 
-tasks.jar {
-    enabled = false
+    tasks.jar {
+        enabled = false
+    }
 }
---
+++ hexagonal/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    kotlin("plugin.spring") version "2.1.0"
+    kotlin("plugin.jpa") version "2.1.0"
+    id("org.springframework.boot") version "3.4.0"
+    id("io.spring.dependency-management") version "1.1.6"
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("com.h2database:h2")
+}
+
+allOpen {
+    annotation("javax.persistence.Entity")
+    annotation("javax.persistence.MappedSuperclass")
+    annotation("javax.persistence.Embeddable")
+}
